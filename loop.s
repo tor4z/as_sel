7de3120f679f02e59d0001f4ebe10b5f8a00903d
tor4z <vwenjie@hotmail.com>
2018-06-18 22:10:20 +0800

    Update loop, add error-checking

--- loop.s
+++ loop.s
@@ -5,16 +5,20 @@ output:
 	.section .text
 	.globl _start
 _start:
-	movl $100, %ecx
-	movl $0, %eax
-sum:	
+	movl $100, %ecx	# Set the loop times
+	movl $0, %eax	# Set the initial value
+sum:
+	jcxz done	# Jump to done if the value in ECX is equal to 0.
 	addl %ecx, %eax
 	loop sum
+
+done:
+	# Print the result
 	pushl %eax
 	pushl $output
 	call printf
-	addl $8, %esp
-
+	addl $8, %esp	# Restore the stack pointer
+	# Exit
 	movl $1, %eax
 	movl $0, %ebx
 	int $0x80

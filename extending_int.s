151c7bf05c4ece8c1d1053bb46ef769856ddbef2
tor4z <vwenjie@hotmail.com>
2018-06-20 01:38:46 +0800

    Update comment

--- extending_int.s
+++ extending_int.s
@@ -5,13 +5,13 @@ _start:
 	movl $279, %ecx		# Set the value of ECX to 0x117
 	movzx %cl, %ebx		# The value of EBX is 0x17
 
-	# Clear register
+	# Clean register
 	movl $0, %ecx
 	movl $0, %ebx
 
 	# Extending signed integer test
 	movw $-79, %cx		# Set the value of CX to 0xffb1
-	movl $0, %ebx		# Clear EBX
+	movl $0, %ebx		# Clean EBX
 	movw %cx, %bx		# The value of BX is 0xffb1
 	movsx %cx, %eax		# Extending the value of CX from word to doubleword,
 				# and store to EAX, the value of EAX is 0xffffffb1

fd64bec151ead4e59aa1c1e51a0ab107c55e3c88
tor4z <vwenjie@hotmail.com>
2018-06-27 18:52:13 +0800

    Add separate function test

---
+++ sep_func/main.s
@@ -0,0 +1,23 @@
+	.section .data
+precision:
+	.byte 0x7f, 0x00
+
+	.section .bss
+	.lcomm result, 4
+
+	.section .text
+	.globl _start
+_start:
+	finit
+	fldcw precision
+
+	pushl $10
+	call area
+	addl $4, %esp
+	movl %eax, result
+
+	movl $1, %eax
+	movl $0, %ebx
+	int $0x80
+	
+	
